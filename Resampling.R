8bccd469bb971b0a03cb137c7b2a397326107650
Yao Cui <aprilcui11@gmail.com>
2016-02-23 09:08:28 -0600

    boot

--- Resampling.R
+++ Resampling.R
@@ -33,3 +33,30 @@ for(d in degree){
 }
 lines(degree,cv.error10,type="b",col="red")
 
+## Bootstrap
+## Minimum risk investment - Section 5.2
+
+alpha=function(x,y){
+  vx=var(x)
+  vy=var(y)
+  cxy=cov(x,y)
+  (vy-cxy)/(vx+vy-2*cxy)
+}
+alpha(Portfolio$X,Portfolio$Y)
+
+## What is the standard error of alpha?
+
+alpha.fn=function(data, index){
+  with(data[index,],alpha(X,Y))
+}
+
+alpha.fn(Portfolio,1:100)
+
+set.seed(1)
+alpha.fn (Portfolio,sample(1:100,100,replace=TRUE))
+
+boot.out=boot(Portfolio,alpha.fn,R=1000)
+boot.out
+plot(boot.out)
+?boot
+?Portfolio
